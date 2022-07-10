629f300efaf977ded27425e50a154ad9c9a6e5ad
Kawauso <daikon16@icloud.com>
2022-07-10 20:01:44 +0900

    Hot: initial revision

---
+++ HoT.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+class HoT{
+public static void main(String[] args){
+    int i,x=0,h=0,t=0;
+    Random rnd = new Random(); 
+System.out.println("Tossing a coin...");
+for(i=1;i<=3;i++){
+x = rnd.nextInt(2);
+System.out.printf("Round"+i+":");
+if(x==1){
+    h++;
+    System.out.printf("Heads\n");
+}
+else{
+    t++;
+    System.out.printf("Tails\n");
+}
+}
+System.out.println("Heads:"+h+",Tails:"+t);
+}
+
+}
